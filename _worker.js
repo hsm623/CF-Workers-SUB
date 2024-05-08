4e1428c7823f3004c8ef82c86d3c60f05827632d
hsm623 <36944029+hsm623@users.noreply.github.com>
2024-05-08 13:08:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,12 @@
 
-// 部署完成后在网址后面加上这个，获取自建节点和机场聚合节点，/?token=auto或/auto或
+// 部署完成后在网址后面输入/，加上下方设置的mytoken=的值，即可获取聚合后的节点
 
-let mytoken = 'auto'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
-let BotToken =''; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
-let ChatID =''; //可以为空，或者@userinfobot中获取，/start
-let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
-let FileName = 'CF-Workers-SUB';
-let SUBUpdateTime = 6; //自定义订阅更新时间，单位小时
+let mytoken = 'top'; //可根据需要自行设定，或者使用UUID在线生成网站进行生成：https://1024tools.com/uuid
+let BotToken ='7067766609:AAEpOc1eRGkT1FInCI-6QG9ogYNo2imuutY'; //可以为空，或电报@BotFather关注机器人，输入/start，选择：/newbot进行获取
+let ChatID ='595873643'; //可以为空，或电报@userinfobot中获取，选择：/start进行获取
+let TG = 0; //新手勿动， 开发者选项，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
+let FileName = 'Pages-SUB-Convert';
+let SUBUpdateTime = 3; //更改此次设置订阅更新时间，单位小时
 
 //节点链接 + 订阅链接
 let MainData = `
